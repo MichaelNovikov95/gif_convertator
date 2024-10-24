e8646a1e5810f73e7aab9de1075e06fbe365d2af
Michael Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2024-10-24 21:35:07 +0300

    updates
    stable version

--- backend/src/app/server.ts
+++ backend/src/app/server.ts
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.get("/", () => {
-  console.log("check");
+  console.log("checks");
 });
 app.use("/api/videos", videoRoutes);
 
