f5781f8d651a304dd2b9e1d9a8014ec0b4783f26
Michael Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2024-10-29 14:14:05 +0200

    last push

--- workers/videoWorker.ts
+++ workers/videoWorker.ts
@@ -1,54 +1,44 @@
 import { Worker, Job, Queue } from "bullmq";
-import { redisConnection } from "../utils/redis.js";
-import { convertVideoToGIF } from "../backend/src/app/services/video.service";
+import { redisConnection } from "./utils/redis";
+import { convertVideoToGIF } from "./services/video.service";
 
 const videoQueue = new Queue("videoQueue", { connection: redisConnection });
 
-const worker = new Worker("videoQueue", async (job: Job) => {
-  const { videoPath } = job.data;
+const worker = new Worker(
+  "videoQueue",
+  async (job: Job) => {
+    const { videoPath } = job.data;
 
-  try {
-    console.log(`Processing video: ${videoPath}`);
+    try {
+      console.log(`Processing video: ${videoPath}`);
 
-    const gifPath = await convertVideoToGIF(videoPath);
+      if (!job.id) return;
 
-    if (job.id) {
+      const gifPath = await convertVideoToGIF(videoPath);
+
       await redisConnection.set(
         job.id,
         JSON.stringify({ status: "completed", outputPath: gifPath })
       );
+
+      console.log(`Video converted to GIF: ${gifPath}`);
+      return gifPath;
+    } catch (error: any) {
+      console.error("Error processing job:", error);
+      if (job.id) {
+        await redisConnection.set(
+          job.id,
+          JSON.stringify({ status: "failed", error: error.message })
+        );
+      }
+      throw error;
     }
-
-    console.log(`Video converted to GIF: ${gifPath}`);
-    return gifPath;
-  } catch (error: any) {
-    console.error("Error processing job:", error);
-    if (job.id) {
-      await redisConnection.set(
-        job.id,
-        JSON.stringify({ status: "failed", error: error.message })
-      );
-    }
-    throw error;
-  }
-});
-
-worker.on("completed", (job: Job) => {
-  console.log(`Job ${job.id} completed with output: ${job.returnvalue}`);
-});
-
-worker.on("failed", (job: Job | undefined, err: Error) => {
-  if (job) {
-    console.log(`Job ${job.id} failed: ${err.message}`);
-  } else {
-    console.log(
-      `A job failed, but the job information is undefined. Error: ${err.message}`
-    );
-  }
-});
+  },
+  { connection: redisConnection }
+);
 
 async function restoreJobs() {
-  const jobs = await videoQueue.getJobs(["waiting", "paused", "failed"]);
+  const jobs = await videoQueue.getJobs(["waiting", "active", "completed"]);
   for (const job of jobs) {
     if (job.name === "convert" && job.data.videoPath) {
       console.log(`Restoring job: ${job.id}, videoPath: ${job.data.videoPath}`);
