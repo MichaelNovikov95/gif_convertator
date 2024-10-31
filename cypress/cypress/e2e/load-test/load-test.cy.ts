98b21b24e1d6961ca9cdea9fe0ecd47bbef2857d
Michael Novikov <100936797+MichaelNovikov95@users.noreply.github.com>
2024-10-31 13:58:47 +0200

    stable load testing passed

--- cypress/cypress/e2e/load-test/load-test.cy.ts
+++ cypress/cypress/e2e/load-test/load-test.cy.ts
@@ -1,24 +1,37 @@
 /// <reference types="cypress" />
 
 describe("Load testing", () => {
-  it("should load", () => {
+  it("should load 100 times", () => {
     const videoFilePath = "demo.mp4";
+    const numberOfRepetitions = 100;
+    const requests = [];
 
-    cy.fixture(videoFilePath, "base64").then((fileContent) => {
-      const blob = Cypress.Blob.base64StringToBlob(fileContent, "image/gif");
-      const formData = new FormData();
-      formData.append("video", blob);
+    for (let i = 0; i < numberOfRepetitions; i++) {
+      requests.push(
+        cy.fixture(videoFilePath, "base64").then((fileContent) => {
+          const blob = Cypress.Blob.base64StringToBlob(
+            fileContent,
+            "video/mp4"
+          );
+          const formData = new FormData();
+          formData.append("video", blob);
 
-      return cy.request({
-        method: "POST",
-        url: "/api/videos/convert",
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-        body: formData,
-        failOnStatusCode: false,
-        timeout: 600000,
-      });
+          return cy.request({
+            method: "POST",
+            url: "/api/videos/convert",
+            headers: {
+              "Content-Type": "multipart/form-data",
+            },
+            body: formData,
+            failOnStatusCode: false,
+            timeout: 600000,
+          });
+        })
+      );
+    }
+
+    Cypress.Promise.all(requests).then((responses) => {
+      console.log("All requests completed:", responses);
     });
   });
 });
